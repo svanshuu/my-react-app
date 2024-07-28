512acb8fc5d26d7d88da61e6aefa28fd3623b137
svanshuu <svanshu200331@gmail.com>
2024-07-28 20:17:17 +0530

    Local

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <button onClick={()=>{}}> Next Page</button>
+      Learn React By Vanshika
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+ function Home() {
+
+return(<div>
+ Hello Dear, This is my first page as Home page....
+
+ <button onClick={()=>{}}> Next Page</button>
+</div>
+)};
+
+export default Home;
